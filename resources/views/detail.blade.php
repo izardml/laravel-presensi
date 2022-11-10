b1c8f6817e7e059e1fdbc50e94c399b1553f5df0
Nizar Daffa <nizartokuzet@gmail.com>
2022-11-10 16:50:28 +0700

    Desain Halaman Siswa (Belum Selesai)

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,57 +1,83 @@
 @extends('master')
 
 @section('body')
+<div class="lg:flex">
     @include('sidebar')
 
-    <h1>Main</h1>
+    {{-- <h1>Main</h1> --}}
 
-    <a href="/guru">Back</a>
+    <div class="lg:w-5/6">
+        <div class="w-full h-10 bg-blue-500 lg:hidden">
+            <button id="hamburger" name="hamburger" type="button" class="block absolute left-4">
+                <span class="hamburger-line transition duration-300 ease-in-out origin-top-left"></span>
+                <span class="hamburger-line transition duration-300 ease-in-out"></span>
+                <span class="hamburger-line transition duration-300 ease-in-out origin-bottom-left"></span>
+            </button>
+        </div>
+        
+        <div class="px-3 py-10 lg:px-6" id="main">
+            {{-- <a href="/guru"><button class="text-xs bg-blue-500 text-slate-100 px-2 py-1 mb-3 rounded-md hover:opacity-80">Back</button></a> --}}
+            <div class="mb-10">
+                <h2 class="text-2xl font-normal mb-4 text-zinc-800">Detail</h2>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Nama Guru</td>
+                            <td>: {{ $attendance->teacher->name }}</td>
+                        </tr>
+                        <tr>
+                            <td>Mapel</td>
+                            <td>: {{ $attendance->subject->name }}</td>
+                        </tr>
+                        <tr>
+                            <td>Kelas</td>
+                            <td>: {{ $attendance->kelas->name }}</td>
+                        </tr>
+                        <tr>
+                            <td>Date</td>
+                            <td>: {{ $attendance->date }}</td>
+                        </tr>
+                        <tr>
+                            <td>Topik</td>
+                            <td>: {{ $attendance->topic }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="mb-10">
+                <h2 class="text-2xl font-normal mb-4 text-zinc-800">Daftar Siswa</h2>
+                <div class="overflow-scroll">
+                    <table class="w-full">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Tanggal</th>
+                                <th>Nama Siswa</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($attdetails as $attdetail)
+                            <tr>
+                                <td class="text-center">{{ $attdetail->id }}</td>
+                                <td class="text-center">{{ date("d M Y", strtotime($attdetail->attendance->date)) }}</td>
+                                <td>{{ $attdetail->student->name }}</td>
+                                <td class="text-center">
+                                    @if($attdetail->attstatus == "tanpaKeterangan")
+                                        Belum Presensi
+                                    @else
+                                        {{ $attdetail->attstatus }}
+                                    @endif
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-    <h2>Attendance Detail</h2>
-    <table>
-        <tbody>
-            <tr>
-                <td>Nama Guru</td>
-                <td>: {{ $attendance->teacher->name }}</td>
-            </tr>
-            <tr>
-                <td>Mapel</td>
-                <td>: {{ $attendance->subject->name }}</td>
-            </tr>
-            <tr>
-                <td>Kelas</td>
-                <td>: {{ $attendance->kelas->name }}</td>
-            </tr>
-            <tr>
-                <td>Date</td>
-                <td>: {{ $attendance->date }}</td>
-            </tr>
-            <tr>
-                <td>Topik</td>
-                <td>: {{ $attendance->topic }}</td>
-            </tr>
-        </tbody>
-    </table>
-
-    <h2>Yang Sudah Hadir</h2>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Tanggal</th>
-                <th>Nama Siswa</th>
-                <th>Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($attdetails as $attdetail)
-            <tr>
-                <td>{{ $attdetail->id }}</td>
-                <td>{{ $attdetail->attendance->date }}</td>
-                <td>{{ $attdetail->student->name }}</td>
-                <td>{{ $attdetail->attstatus }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    @include('script')
 @endsection
--- resources/views/siswa.blade.php
+++ resources/views/siswa.blade.php
@@ -1,82 +1,100 @@
 @extends('master')
 
 @section('body')
+<div class="lg:flex">
     @include('sidebar')
 
-    <h1>Main</h1>
+    {{-- <h1>Main</h1> --}}
+    <div class="lg:w-5/6">
+        <div class="w-full h-10 bg-blue-500 lg:hidden">
+            <button id="hamburger" name="hamburger" type="button" class="block absolute left-4">
+                <span class="hamburger-line transition duration-300 ease-in-out origin-top-left"></span>
+                <span class="hamburger-line transition duration-300 ease-in-out"></span>
+                <span class="hamburger-line transition duration-300 ease-in-out origin-bottom-left"></span>
+            </button>
+        </div>
 
-    <h2>Presensi Hari Ini</h2>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama Guru</th>
-                <th>Mapel</th>
-                <th>Kelas</th>
-                <th>Tanggal</th>
-                <th>Topik</th>
-                <th>Nama Siswa</th>
-                <th>Kehadiran</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($atttodays as $attendance)
-            <tr>
-                <td>{{ $attendance->id }}</td>
-                <td>{{ $attendance->attendance->teacher->name }}</td>
-                <td>{{ $attendance->attendance->subject->name }}</td>
-                <td>{{ $attendance->attendance->kelas->name }}</td>
-                <td>{{ $attendance->attendance->date }}</td>
-                <td>{{ $attendance->attendance->topic }}</td>
-                <td>{{ $attendance->student->name }}</td>
-                {{-- <td>{{ if($attendance->attstatus == 'tanpaKeterangan') ? echo 'Belum Presensi' : $attendance->attstatus }}</td> --}}
-                <td>@if ($attendance->attstatus == 'tanpaKeterangan')
-                    Belum Presensi
-                @else
-                    {{$attendance->attstatus}}
-                @endif</td>
-                <td>
-                    <a href="{{ route('siswa.edit', $attendance->id) }}"><button>Submit</button></a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+        <div class="px-3 py-10 lg:px-6" id="main">
+            <div class="mb-10">
+                <h2 class="text-2xl font-normal mb-4 text-zinc-800">Presensi Hari Ini</h2>
+                <div class="overflow-scroll">
+                    <table class="w-full">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Guru</th>
+                                <th>Mapel</th>
+                                <th>Kelas</th>
+                                <th>Tanggal</th>
+                                <th>Topik</th>
+                                <th>Nama Siswa</th>
+                                <th>Kehadiran</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($atttodays as $attendance)
+                            <tr>
+                                <td class="text-center">{{ $attendance->id }}</td>
+                                <td>{{ $attendance->attendance->teacher->name }}</td>
+                                <td>{{ $attendance->attendance->subject->name }}</td>
+                                <td class="text-center">{{ $attendance->attendance->kelas->name }}</td>
+                                <td class="text-center">{{ date("d M Y", strtotime($attendance->attendance->date)) }}</td>
+                                <td>{{ $attendance->attendance->topic }}</td>
+                                <td>{{ $attendance->student->name }}</td>
+                                {{-- <td>{{ $attendance->attstatus }}</td> --}}
+                                <td>@if ($attendance->attstatus == 'tanpaKeterangan')
+                                    Belum Presensi
+                                @else
+                                    {{$attendance->attstatus}}
+                                @endif</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="mb-10">
+                <h2 class="text-2xl font-normal mb-4 text-zinc-800">Daftar Presensi</h2>
+                <div class="overflow-scroll">
+                    <table class="w-full">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Guru</th>
+                                <th>Mapel</th>
+                                <th>Kelas</th>
+                                <th>Tanggal</th>
+                                <th>Topik</th>
+                                <th>Nama Siswa</th>
+                                <th>Kehadiran</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($attendances as $attendance)
+                            <tr>
+                                <td class="text-center">{{ $attendance->id }}</td>
+                                <td>{{ $attendance->attendance->teacher->name }}</td>
+                                <td>{{ $attendance->attendance->subject->name }}</td>
+                                <td class="text-center">{{ $attendance->attendance->kelas->name }}</td>
+                                <td class="text-center">{{ date("d M Y", strtotime($attendance->attendance->date)) }}</td>
+                                <td>{{ $attendance->attendance->topic }}</td>
+                                <td>{{ $attendance->student->name }}</td>
+                                {{-- <td>{{ $attendance->attstatus }}</td> --}}
+                                <td>@if ($attendance->attstatus == 'tanpaKeterangan')
+                                    Belum Presensi
+                                @else
+                                    {{$attendance->attstatus}}
+                                @endif</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-    <h2>Daftar Presensi</h2>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama Guru</th>
-                <th>Mapel</th>
-                <th>Kelas</th>
-                <th>Tanggal</th>
-                <th>Topik</th>
-                <th>Nama Siswa</th>
-                <th>Kehadiran</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($attendances as $attendance)
-            <tr>
-                <td>{{ $attendance->id }}</td>
-                <td>{{ $attendance->attendance->teacher->name }}</td>
-                <td>{{ $attendance->attendance->subject->name }}</td>
-                <td>{{ $attendance->attendance->kelas->name }}</td>
-                <td>{{ $attendance->attendance->date }}</td>
-                <td>{{ $attendance->attendance->topic }}</td>
-                <td>{{ $attendance->student->name }}</td>
-                {{-- <td>{{ $attendance->attstatus }}</td> --}}
-                <td>@if ($attendance->attstatus == 'tanpaKeterangan')
-                    Belum Presensi
-                @else
-                    {{$attendance->attstatus}}
-                @endif</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-
+    @include('script')
 @endsection
