04a4ff08f3dc4b49f5564f223a1eac5271fa0cd6
Nizar Daffa <nizartokuzet@gmail.com>
2022-11-10 10:40:04 +0700

    Tailwind CSS

--- resources/views/guru.blade.php
+++ resources/views/guru.blade.php
@@ -1,27 +1,27 @@
 @extends('master')
 
 @section('body')
-    @include('sidebar')
+    {{-- @include('sidebar') --}}
 
-    <h1>Main</h1>
-
-    <h2>Form Buat Presensi</h2>
+    {{-- <h1>Main</h1> --}}
+<div class="px-3 py-10">
+    <h2 class="text-xl font-medium mb-2">Form Buat Presensi</h2>
     <form action="/guru" method="post">
         @csrf
-        <select name="subject_id" id="subject_id">
+        <select name="subject_id" id="subject_id" class="text-xs">
             <option value="">Pilih Mapel</option>
             @foreach ($subjects as $subject)
             <option value="{{ $subject->id }}">{{ $subject->name }}</option>
             @endforeach
         </select>
-        <select name="kelas_id" id="kelas_id">
+        <select name="kelas_id" id="kelas_id" class="text-xs">
             <option value="">Pilih Kelas</option>
             @foreach ($kelass as $kelas)
             <option value="{{ $kelas->id }}">{{ $kelas->name }}</option>
             @endforeach
         </select>
-        <input type="text" name="topic" id="topic" placeholder="Masukkan topik...">
-        <input type="submit" value="Submit">
+        <input type="text" name="topic" id="topic" placeholder="Masukkan topik..." class="bg-transparent border-b border-slate-300 text-xs mb-2 p-1">
+        <input type="submit" value="Submit" class="bg-blue-500 text-slate-100 text-[10px] px-2 py-1 rounded-md hover:opacity-80">
     </form>
 
     <h2>Daftar Presensi</h2>
@@ -55,4 +55,5 @@
             @endforeach
         </tbody>
     </table>
+</div>
 @endsection
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,12 @@
 @extends('master')
 
 @section('body')
-    <div class="row d-flex justify-content-center">
-        <form action="/login" method="post" class="border border-5 border-secondary p-5 col-3 rounded">
+    <div class="bg-blue-50 flex justify-center items-center h-screen ">
+        <form action="/login" method="post" class="w-4/5 ">
+            <h1 class="text-center text-2xl mb-6">Login Dulu</h1>
             @csrf
-            <label for="email" class="form-label">Email</label>
-            <input type="email" name="email" id="email" placeholder="Masukkan email..." value="{{ Session::get('email') }}" required class="form-control"><br>
+            <label for="email" class="hidden">Email</label>
+            <input type="email" name="email" id="email" placeholder="Masukkan email..." value="{{ Session::get('email') }}" required class="bg-transparent border-b border-slate-300 mb-2 p-1 w-full"><br>
             {{-- <select name="email" id="email">
                 <option value="">Pilih email</option>
                 @foreach ($users as $user)
@@ -13,11 +14,10 @@
                 @endforeach
             </select><br> --}}
 
+            <label for="password" class="hidden">Password</label>
+            <input type="password" name="password" id="password" placeholder="Masukkan password..." required class="bg-transparent border-b border-slate-300 mb-2 p-1 w-full"><br>
 
-            <label for="password" class="form-label">Password</label>
-            <input type="password" name="password" id="password" placeholder="Masukkan password..." required class="form-control"><br>
-
-            <input type="submit" value="Login" class="btn btn-primary">
+            <input type="submit" value="Login" class="bg-blue-500 text-slate-100 text-sm p-2 rounded-md w-full hover:opacity-80">
         </form>
     </div>
 @endsection
